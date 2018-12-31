44cc18ba27b87b9d8eb81d0da1ddc57ab8542f21
seshi <seshi_kiran82@yahoo.co.in>
2018-12-31 05:29:37 +0000

    sorting by type

--- src/main/java/hello/springboot/HelloController.java
+++ src/main/java/hello/springboot/HelloController.java
@@ -2,6 +2,8 @@ package hello.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -36,5 +38,65 @@ public class HelloController {
         
         return "Collections Sorted Array " + list;
     }
+    
+    @RequestMapping("/hellosortbytype")
+    public String sortByType(String[] strArr,SortingType sortingType) {
 
+    if(sortingType.equals(SortingType.BUBBLE_SORT)){
+ 
+     int[] arr = new int[strArr.length];
+ 
+     for(int s=0;s<strArr.length;s++){
+        arr[s] = Integer.parseInt(strArr[s]);
+     } 
+ 
+      boolean swapped = true;
+      int j=0;
+      int tmp;
+      
+      while(swapped){
+          
+          swapped= false;
+          j++;
+          for(int i=0; i<arr.length-j; i++){
+                      
+              if(arr[i] > arr[i+1]){
+                  tmp= arr[i];
+                  arr[i]=arr[i+1];
+                  arr[i]=tmp;
+                  swapped=true;
+              }
+          }
+          
+      }
+      
+      List<String> sortedArr = new ArrayList<String>();
+      
+      for(int a=0;a<arr.length;a++){
+        sortedArr.add(String.valueOf(arr[a]));  
+      }
+      
+      
+       
+       return " Bubble Sort  Result "+sortedArr;
+       
+       }else{
+           
+        List<String> list = Arrays.asList(strArr);
+
+        System.out.println("Input array List " + list);
+
+        Collections.sort(list);
+        
+        System.out.println("Collections Sorted list " + list);
+        
+        Arrays.sort(strArr);
+
+        System.out.println("Input array sort " + Arrays.asList(strArr));
+        
+        return "Default Collections Sorted Array " + list;
+        
+        }
+    }
+    
 }
---
+++ src/main/java/hello/springboot/SortingType.java
@@ -0,0 +1,7 @@
+package hello.springboot;
+
+public enum SortingType {
+    
+    BUBBLE_SORT, QUICK_SORT, MERGE_SORT
+    
+}
